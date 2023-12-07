f2c33f1c780430f9641b998dca806fc2e4e153c6
Ketan Yeluri <e0603110@u.nus.edu>
2023-12-08 03:06:46 +0800

    Add LeetCode 2667, 2677, 2695

---
+++ LeetCode/2601-2700/2677. Chunk Array/Solution.ts
@@ -0,0 +1,16 @@
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | Array<JSONValue>;
+
+function chunk(arr: Obj[], size: number): Obj[][] {
+  var result: Obj[][] = [];
+  for (let i = 0; i < arr.length; i += size) {
+    result.push(arr.slice(i, i + size));
+  }
+  return result;
+}
